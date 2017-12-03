72eb79043bfd7b03f4eaa6937876372b85a76117
andreas <andreas.luethi@gmx.net>
2017-12-03 21:19:04 +0100

    Implement todo, wip.

--- src/ubx/a/b/ProcessData.java
+++ src/ubx/a/b/ProcessData.java
@@ -7,48 +7,53 @@ import java.util.List;
 public class ProcessData {
 
     private static final DecimalFormat form3 = new DecimalFormat("0.000");
-
+    private static final String SEP = ",";
+    private static List<Vrec> vRecs = null;
 
     public static List<String> getCSVRecs(List<Rrec> rRecs) {
         List<String> csvRecs = new ArrayList<String>();
-        List<Vrec> vRecs = new ArrayList<Vrec>();
+        vRecs = new ArrayList<Vrec>();
+        List<Vrec> vRecsShort = new ArrayList<Vrec>();
         Vrec vrecLast = null;
         int cnt = 1;
         for (Rrec rrec : rRecs) {
-            Vrec vrec = new Vrec();
-            String strs[] = rrec.gs.split(",");
-            // --GS,PN1,N 1200261.6916,E 2608973.7195,EL583.6008,--
-            vrec.easting = Double.valueOf(strs[2].split(" ")[1]);
-            vrec.northing = Double.valueOf(strs[3].split(" ")[1]);
-            vrec.elevation = Float.valueOf(strs[4].substring(2));
-            // --HSDV:0.011, VSDV:0.014, STATUS:FIXED, SATS:13, AGE:0.6, PDOP:1.853, HDOP:1.100, VDOP:1.491, TDOP:1.116, GDOP:1.479, NSDV
-            strs = rrec.hsdv.split(",");
-            vrec.hsdv = Float.valueOf(strs[0].split(":")[1]);
-            vrec.vsdv = Float.valueOf(strs[1].split(":")[1]);
-            vrec.pdop = Float.valueOf(strs[7].split(":")[1]);
-            // --DT10-01-2015
-            // --TM00:04:50
-            vrec.date = rrec.dt.substring(4);
-            vrec.time = rrec.tm.substring(4);
-            if (vrecLast != null) {
-                System.out.println("dist" + cnt + "=" + distance(vrecLast, vrec));
+            Vrec vrec = getVrec(rrec);
+            if (vrecLast == null) {
+                vRecs.add(vrec);
+            } else {
+                if (distance(vrecLast, vrec) < 0.5) {
+                    vRecsShort.add(vrec);
+                } else {
+                    average(vRecs.get(vRecs.size() - 1), vRecsShort);
+                    vRecsShort.clear();
+                    vRecs.add(vrec);
+                }
             }
-            vRecs.add(vrec);
             vrecLast = vrec;
 
-            // todo:
-            //   loop over vRecs
-            //   calculate distance between points
-            //   calculate "points average" position
-            //   create an new vRecs list with reduced/additional points
-            //   add uniques names
-            //   create a new csvRecs aand return it
-
-
-            csvRecs.add("GCP" + cnt++ + "," + f3(vrec.easting) + "," + f3(vrec.northing) + "," + f3(vrec.elevation)
-                    + "," + f3(vrec.hsdv) + "," + f3(vrec.vsdv));
+            csvRecs.add("GCP" + cnt++ + SEP + f3(vrec.easting) + SEP + f3(vrec.northing) + SEP + f3(vrec.elevation)
+                    + SEP + f3(vrec.hsdv) + SEP + f3(vrec.vsdv));
         }
         return csvRecs;
+    }
+
+    private static Vrec getVrec(Rrec rrec) {
+        Vrec vrec = new Vrec();
+        String strs[] = rrec.gs.split(",");
+        // --GS,PN1,N 1200261.6916,E 2608973.7195,EL583.6008,--
+        vrec.easting = Double.valueOf(strs[2].split(" ")[1]);
+        vrec.northing = Double.valueOf(strs[3].split(" ")[1]);
+        vrec.elevation = Float.valueOf(strs[4].substring(2));
+        // --HSDV:0.011, VSDV:0.014, STATUS:FIXED, SATS:13, AGE:0.6, PDOP:1.853, HDOP:1.100, VDOP:1.491, TDOP:1.116, GDOP:1.479, NSDV
+        strs = rrec.hsdv.split(",");
+        vrec.hsdv = Float.valueOf(strs[0].split(":")[1]);
+        vrec.vsdv = Float.valueOf(strs[1].split(":")[1]);
+        vrec.pdop = Float.valueOf(strs[7].split(":")[1]);
+        // --DT10-01-2015
+        // --TM00:04:50
+        vrec.date = rrec.dt.substring(4);
+        vrec.time = rrec.tm.substring(4);
+        return vrec;
     }
 
 
@@ -64,18 +69,17 @@ public class ProcessData {
         return Math.sqrt(Math.pow(v0.easting - v1.easting, 2) + Math.pow(v0.northing - v1.northing, 2));
 
     }
-
-    private static double distance(double e0, double e1, double n0, double n1) {
-        return Math.sqrt(Math.pow(e1 - e0, 2) + Math.pow(n1 - n0, 2));
-
-    }
-
-    private static double average(double... vals) {
-        double sum = 0.0;
-        for (double val : vals) {
-            sum = sum + val;
+    private static void average(Vrec vrec, List<Vrec> vRecsShort) {
+        vrec.numberOfMeasurements = 1 + vRecsShort.size();
+        if (vRecsShort.size() == 0) return;
+        for (Vrec vr : vRecsShort) {
+            vrec.easting += vr.easting;
+            vrec.northing += vr.northing;
+            vrec.elevation += vr.elevation;
         }
-        return sum / vals.length;
+        vrec.easting /= vrec.numberOfMeasurements;
+        vrec.northing /= vrec.numberOfMeasurements;
+        vrec.elevation /= vrec.numberOfMeasurements;
     }
 
 
