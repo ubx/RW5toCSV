2bb42669488f90aeb4e9f25de56eda6eaee945b3
andreas <andreas.luethi@gmx.net>
2017-12-05 11:39:01 +0100

    Add junit tests.

---
+++ src/ch/luethi/rw5tocsv/RA5toCSVTest.java
@@ -0,0 +1,18 @@
+package ch.luethi.rw5tocsv;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RA5toCSVTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+}
