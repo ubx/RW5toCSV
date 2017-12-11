c5be7c09ae4b6282408213772d4c1fbdce6216a8
andreas <andreas.luethi@gmx.net>
2017-12-11 19:11:34 +0100

    Add more junit tests #2.

--- src/ch/luethi/rw5tocsv/RA5toCSVTest.java
+++ src/ch/luethi/rw5tocsv/RA5toCSVTest.java
@@ -12,6 +12,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class RA5toCSVTest {
 
+    private static final String THE_FILES_DIFFER = "The files differ!";
+
     private static final String TESTDATA = "testdata";
     private static final String BERNECK_RW5 = TESTDATA + "/BERNECK.rw5";
     private static final String BERNECK_CSV = TESTDATA + "/BERNECK.csv";
@@ -27,8 +29,10 @@ class RA5toCSVTest {
     private static final String FloatingFormatError_CSV = TESTDATA + "/test-01.csv";
     private static final String FloatingFormatError_REF_CSV = TESTDATA + "/test-01-ref.csv";
 
-
-    private static final String TEST_CSV = TESTDATA + "/testA.csv";
+    private static final String DriftExceedsLimits_RW5 = TESTDATA + "/P301-12_AARE.rw5";
+    private static final String DriftExceedsLimits_CSV = TESTDATA + "/P301-12_AARE.csv";
+    private static final String DriftExceedsLimits_TXT = TESTDATA + "/P301-12_AARE.txt";
+    private static final String DriftExceedsLimits_REF_TXT = TESTDATA + "/P301-12_AARE-ref.txt";
 
     @BeforeEach
     void setUp() {
@@ -43,22 +47,28 @@ class RA5toCSVTest {
         deleteQuietly(new File(BERNECK_CSV));
         deleteQuietly(new File(BERNECK_TXT));
         RA5toCSV.main(new String[]{"-r", BERNECK_RW5, "-c", BERNECK_CSV, "-t", BERNECK_TXT});
-        assertTrue(contentEquals(new File(BERNECK_CSV), new File(BERNECK_REF_CSV)), "The files differ!");
-        assertTrue(contentEquals(new File(BERNECK_TXT), new File(BERNECK_REF_TXT)), "The files differ!");
+        assertTrue(contentEquals(new File(BERNECK_CSV), new File(BERNECK_REF_CSV)), THE_FILES_DIFFER);
+        assertTrue(contentEquals(new File(BERNECK_TXT), new File(BERNECK_REF_TXT)), THE_FILES_DIFFER);
     }
 
     @Test
     void formatErrorTest() throws IOException {
         deleteQuietly(new File(FORMARERROR_CSV));
         RA5toCSV.main(new String[]{"-r", FORMARERROR_RW5, "-c", FORMARERROR_CSV});
-        assertTrue(contentEquals(new File(FORMARERROR_CSV), new File(FORMARERROR_REF_CSV)), "The files differ!");
+        assertTrue(contentEquals(new File(FORMARERROR_CSV), new File(FORMARERROR_REF_CSV)), THE_FILES_DIFFER);
     }
 
     @Test
     void FloatingFormatErrorTest() throws IOException {
         deleteQuietly(new File(FloatingFormatError_CSV));
         RA5toCSV.main(new String[]{"-r", FloatingFormatError_RW5, "-c", FloatingFormatError_CSV});
-        assertTrue(contentEquals(new File(FloatingFormatError_CSV), new File(FloatingFormatError_REF_CSV)), "The files differ!");
+        assertTrue(contentEquals(new File(FloatingFormatError_CSV), new File(FloatingFormatError_REF_CSV)), THE_FILES_DIFFER);
+    }
+    @Test
+    void DriftExceedsLimitsTest() throws IOException {
+        deleteQuietly(new File(DriftExceedsLimits_TXT));
+        RA5toCSV.main(new String[]{"-r", DriftExceedsLimits_RW5, "-t", DriftExceedsLimits_TXT, "-c", DriftExceedsLimits_CSV});
+        assertTrue(contentEquals(new File(DriftExceedsLimits_TXT), new File(DriftExceedsLimits_REF_TXT)), THE_FILES_DIFFER);
     }
 
 }
