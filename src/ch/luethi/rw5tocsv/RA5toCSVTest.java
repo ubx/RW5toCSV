5d040a3089eb5b0ff9854d8de24c0e10aa22a707
andreas <andreas.luethi@gmx.net>
2017-12-13 21:07:37 +0100

    Add more junit tests and refactoring.

--- src/ch/luethi/rw5tocsv/RA5toCSVTest.java
+++ src/ch/luethi/rw5tocsv/RA5toCSVTest.java
@@ -12,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class RA5toCSVTest {
 
-    private static final String THE_FILES_DIFFER = "The files differ!";
+    private static final String THE_FILES_DIFFER = " The files differ!";
 
     private static final String TESTDATA = "testdata";
     private static final String BERNECK_RW5 = TESTDATA + "/BERNECK.rw5";
@@ -34,10 +34,15 @@ class RA5toCSVTest {
     private static final String DriftExceedsLimits_TXT = TESTDATA + "/P301-12_AARE.txt";
     private static final String DriftExceedsLimits_REF_TXT = TESTDATA + "/P301-12_AARE-ref.txt";
 
-    private static final String Test002_RW5 = TESTDATA + "/test-02.rw5";
-    private static final String Test002_CSV = TESTDATA + "/test-02.csv";
-    private static final String Test002_TXT = TESTDATA + "/test-02.txt";
-    private static final String Test002_REF_TXT = TESTDATA + "/test-02-ref.txt";
+    private static final String Issue5_RW5 = TESTDATA + "/test-02.rw5";
+    private static final String Issue5_CSV = TESTDATA + "/test-02.csv";
+    private static final String Issue5_TXT = TESTDATA + "/test-02.txt";
+    private static final String Issue5_REF_TXT = TESTDATA + "/test-02-ref.txt";
+
+    private static final String Issue6_RW5 = TESTDATA + "/test-03.rw5";
+    private static final String Issue6_CSV = TESTDATA + "/test-03.csv";
+    private static final String Issue6_TXT = TESTDATA + "/test-03.txt";
+    private static final String Issue6_REF_TXT = TESTDATA + "/test-03-ref.txt";
 
     @BeforeEach
     void setUp() {
@@ -52,37 +57,49 @@ class RA5toCSVTest {
         deleteQuietly(new File(BERNECK_CSV));
         deleteQuietly(new File(BERNECK_TXT));
         RA5toCSV.main(new String[]{"-r", BERNECK_RW5, "-c", BERNECK_CSV, "-t", BERNECK_TXT});
-        assertTrue(contentEquals(new File(BERNECK_CSV), new File(BERNECK_REF_CSV)), THE_FILES_DIFFER);
-        assertTrue(contentEquals(new File(BERNECK_TXT), new File(BERNECK_REF_TXT)), THE_FILES_DIFFER);
+        getaVoid(BERNECK_CSV, BERNECK_REF_CSV);
+        getaVoid(BERNECK_TXT, BERNECK_REF_TXT);
     }
 
     @Test
     void formatErrorTest() throws IOException {
         deleteQuietly(new File(FORMARERROR_CSV));
         RA5toCSV.main(new String[]{"-r", FORMARERROR_RW5, "-c", FORMARERROR_CSV});
-        assertTrue(contentEquals(new File(FORMARERROR_CSV), new File(FORMARERROR_REF_CSV)), THE_FILES_DIFFER);
+        getaVoid(FORMARERROR_CSV, FORMARERROR_REF_CSV);
     }
 
     @Test
     void FloatingFormatErrorTest() throws IOException {
         deleteQuietly(new File(FloatingFormatError_CSV));
         RA5toCSV.main(new String[]{"-r", FloatingFormatError_RW5, "-c", FloatingFormatError_CSV});
-        assertTrue(contentEquals(new File(FloatingFormatError_CSV), new File(FloatingFormatError_REF_CSV)), THE_FILES_DIFFER);
+        getaVoid(FloatingFormatError_CSV, FloatingFormatError_REF_CSV);
     }
 
     @Test
     void DriftExceedsLimitsTest() throws IOException {
         deleteQuietly(new File(DriftExceedsLimits_TXT));
         RA5toCSV.main(new String[]{"-r", DriftExceedsLimits_RW5, "-t", DriftExceedsLimits_TXT, "-c", DriftExceedsLimits_CSV});
-        assertTrue(contentEquals(new File(DriftExceedsLimits_TXT), new File(DriftExceedsLimits_REF_TXT)), THE_FILES_DIFFER);
+        getaVoid(DriftExceedsLimits_TXT, DriftExceedsLimits_REF_TXT);
     }
 
     @Test
     void issue5_Test() throws IOException {
-        deleteQuietly(new File(Test002_TXT));
-        deleteQuietly(new File(Test002_CSV));
-        RA5toCSV.main(new String[]{"-r", Test002_RW5, "-t", Test002_TXT, "-c",Test002_CSV });
-        assertTrue(contentEquals(new File(Test002_TXT), new File(Test002_REF_TXT)), THE_FILES_DIFFER);
+        deleteQuietly(new File(Issue5_TXT));
+        deleteQuietly(new File(Issue5_CSV));
+        RA5toCSV.main(new String[]{"-r", Issue5_RW5, "-t", Issue5_TXT, "-c", Issue5_CSV});
+        getaVoid(Issue5_TXT, Issue5_REF_TXT);
+    }
+
+    @Test
+    void issue6_Test() throws IOException {
+        deleteQuietly(new File(Issue6_TXT));
+        deleteQuietly(new File(Issue6_CSV));
+        RA5toCSV.main(new String[]{"-r", Issue6_RW5, "-t", Issue6_TXT, "-c", Issue6_CSV});
+        getaVoid(Issue6_TXT, Issue6_REF_TXT);
+    }
+
+    private void getaVoid(String file, String file2) throws IOException {
+        assertTrue(contentEquals(new File(file), new File(file2)), file + "," + file2 + THE_FILES_DIFFER);
     }
 
 }
